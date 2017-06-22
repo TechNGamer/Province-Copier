c751a36b08310a0ee4126f5affbf8abcd282210c
TechNGamer <TechNGamer@users.noreply.github.com>
2017-06-21 21:47:26 -0500

    About popup change.
    Changes how the About popup works.

--- Province Copier/UIClasses/AboutPopup.cs
+++ Province Copier/UIClasses/AboutPopup.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Resources;
 
@@ -13,15 +8,15 @@ namespace ProvinceCopier.UIClasses {
 		public AboutPopup() {
 			InitializeComponent();
 			this.Text = String.Format( "About {0}", AssemblyTitle );
-			this.labelProductName.Text = AssemblyProduct;
-			this.labelVersion.Text = String.Format( "Version {0}", AssemblyVersion );
-			this.labelCopyright.Text = AssemblyCopyright;
-			this.labelCompanyName.Text = AssemblyCompany;
-			this.textBoxDescription.Text = AssemblyDescription;
+			this.labelProductName.Text = "Province Copier";
+			this.LabelCreatedBy.Text = "Created by Tech N Gamer";
+			this.LableVersion.Text = $"Version {VersionNum}";
+			this.LicensingType.Text = "MIT License";
+			this.textBoxDescription.Text = "This software was made to help EUIV modders pull provences from the EUIV" +
+				" directory. This software is licensed under the MIT License.";
 		}
 
 		#region Assembly Attribute Accessors
-
 		public string AssemblyTitle {
 			get {
 				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes( typeof( AssemblyTitleAttribute ), false );
@@ -34,50 +29,10 @@ namespace ProvinceCopier.UIClasses {
 				return System.IO.Path.GetFileNameWithoutExtension( Assembly.GetExecutingAssembly().CodeBase );
 			}
 		}
-
-		public string AssemblyVersion {
+		public string VersionNum {
 			get {
-				return Assembly.GetExecutingAssembly().GetName().Version.ToString();
-			}
-		}
-
-		public string AssemblyDescription {
-			get {
-				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes( typeof( AssemblyDescriptionAttribute ), false );
-				if( attributes.Length == 0 ) {
-					return "";
-				}
-				return ( ( AssemblyDescriptionAttribute ) attributes[0] ).Description;
-			}
-		}
-
-		public string AssemblyProduct {
-			get {
-				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes( typeof( AssemblyProductAttribute ), false );
-				if( attributes.Length == 0 ) {
-					return "";
-				}
-				return ( ( AssemblyProductAttribute ) attributes[0] ).Product;
-			}
-		}
-
-		public string AssemblyCopyright {
-			get {
-				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes( typeof( AssemblyCopyrightAttribute ), false );
-				if( attributes.Length == 0 ) {
-					return "";
-				}
-				return ( ( AssemblyCopyrightAttribute ) attributes[0] ).Copyright;
-			}
-		}
-
-		public string AssemblyCompany {
-			get {
-				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes( typeof( AssemblyCompanyAttribute ), false );
-				if( attributes.Length == 0 ) {
-					return "";
-				}
-				return ( ( AssemblyCompanyAttribute ) attributes[0] ).Company;
+				ResourceManager rm = new ResourceManager( "Resources.Strings", GetType().Assembly );
+				return $"{rm.GetString( "VersionMajor" )}.{rm.GetString( "VersionMinor" )}.{rm.GetString( "VersionPatch" )}";
 			}
 		}
 		#endregion
